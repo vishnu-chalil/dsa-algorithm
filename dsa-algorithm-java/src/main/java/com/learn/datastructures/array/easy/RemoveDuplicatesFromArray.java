6b79f6445d2e874e2bae701dac76b77e269742ed
Vishnu C <Vishnu.chalil@litmus7.com>
2024-01-28 00:23:16 +0530

    Anagram problem added

--- dsa-algorithm-java/src/main/java/com/learn/datastructures/array/easy/RemoveDuplicatesFromArray.java
+++ dsa-algorithm-java/src/main/java/com/learn/datastructures/array/easy/RemoveDuplicatesFromArray.java
@@ -1,4 +1,4 @@
-package com.learn.datastructures.array;
+package com.learn.datastructures.array.easy;
 
 /**
  * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
