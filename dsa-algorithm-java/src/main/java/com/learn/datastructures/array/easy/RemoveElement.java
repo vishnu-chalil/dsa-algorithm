a9f0c74f34fc3576ff4a07a1f24bcbc460a5e94e
Vishnu C <Vishnu.chalil@litmus7.com>
2024-01-28 00:19:51 +0530

    Move package of easy problems

--- dsa-algorithm-java/src/main/java/com/learn/datastructures/array/easy/RemoveElement.java
+++ dsa-algorithm-java/src/main/java/com/learn/datastructures/array/easy/RemoveElement.java
@@ -1,4 +1,4 @@
-package com.learn.datastructures.array;
+package com.learn.datastructures.array.easy;
 /**
  * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
  *
